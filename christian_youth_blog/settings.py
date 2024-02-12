2f8d1ca3ad394ac150e8b3a7b20824c04c5f0dbc
Lanre Andero <lanreandero@gmail.com>
2024-02-12 09:36:59 +0000

    add to allowed hosts

--- christian_youth_blog/settings.py
+++ christian_youth_blog/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = [
     "http://127.0.0.1:8000",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "ui.dev/amiresponsive?url=https://cy-django-blog-486df1fc929f.herokuapp.com",
 ]
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
