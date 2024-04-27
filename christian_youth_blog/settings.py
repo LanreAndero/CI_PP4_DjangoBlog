c44be20ae99ea9765b0d37838dba5811bf4cc73b
Lanre Andero <lanreandero@gmail.com>
2024-04-27 08:37:18 +0000

    modified settings.py

--- christian_youth_blog/settings.py
+++ christian_youth_blog/settings.py
@@ -13,9 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 from django.contrib.messages import constants as messages
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api  # noqa
+# import django_heroku
+# import cloudinary
+# import cloudinary.uploader
+# import cloudinary.api
 import dj_database_url
 if os.path.isfile("env.py"):
     import env  # noqa
@@ -26,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'cy-django-blog-486df1fc929f.herokuapp.com',
@@ -91,7 +92,6 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'dmwocs4qe',
     'API_KEY': '134374111236266',
     'API_SECRET': 'Jq2ddAoUbCj59Epng85km0hojOA',
-    'SECURE': 'True',
 }
 
 DJANGO_SUMMERNOTE_CONFIG = {
@@ -125,6 +125,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'allauth.account.middleware.AccountMiddleware',
 ]
 
@@ -209,11 +210,12 @@ MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = (
     'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 )
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
