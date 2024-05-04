c8b0bec63bfe7a7f8b4de6a3a1495ae8d2b1aa29
Lanre Andero <lanreandero@gmail.com>
2024-05-04 16:26:54 +0000

    changed debug to false, updated sitemap.xml and style.css

--- christian_youth_blog/settings.py
+++ christian_youth_blog/settings.py
@@ -11,7 +11,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'cy-django-blog-486df1fc929f.herokuapp.com',
